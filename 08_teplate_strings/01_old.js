8bd238e6017b5958f7a5d0b7a6364d10f525585c
Tiago Bordin <tiagolemespalhano@gmail.com>
2025-09-18 01:25:27 -0300

    Acabado react ES6, continuar em https://www.w3schools.com/react/react_jsx.asp

---
+++ 08_teplate_strings/01_old.js
@@ -0,0 +1,6 @@
+const name = "John";
+const age = 30;
+const message = "Hello, " + name + ", you are " +
+    age + " years old.";
+    
+console.log(message);
---
+++ 08_teplate_strings/03_indentation.js
@@ -0,0 +1,8 @@
+const text = `
+    John: 
+        Hello, Jane! How are you?
+    Jane:
+        I am fine, thanks!
+`
+
+console.log(text);
